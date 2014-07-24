fb75eee71163d633028dd680023e4b6b7ba01bc4
Thannia Blanchet <thannia.blanchet@gmail.com>
2014-07-24 15:54:48 -0700

    still no functionality

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 (function(){
+
 	var app = angular.module("todo", []);
-	var tasks = ["call", "eat", "breathe"];
+	var tasks = ["test", "eat", "breathe"];
+
 	app.controller("TodoController", function (){
+
 			this.tasks = tasks;
 
-			this.clickBox = function(){
-				alert("funciona");
+			this.addTask = function(){
+					alert("add a task");
+			}
+
+			this.clickBox = function(){				
+				alert("cross out list name and substract leftover task");
 			};
+
+			this.deleteBox = function(){
+				alert("delete list item and substract from task list");
+			};
+
+			this.editTask = function(){
+				alert("edit already added task on double click");
+			};
+
 		}
 	);
 
